3a4175397882fe3cb8368e6e1607f345665dc641
Lukas Forst <lukas.forst@zoho.com>
2016-09-16 15:55:53 +0200

    Add comment to sqrt calculation

--- primality-test.cpp
+++ primality-test.cpp
@@ -4,7 +4,7 @@ bool isPrimeBruteForce(int x) {
 	if (x < 2)
 		return false;
 	float sqroot_x = sqrt(x);
-	for(int i=0; i <= sqroot_x; i++) {
+	for(int i=0; i <= sqroot_x; i++) { /* If there were only factors above the square root of x, they would be bigger than x itself. */
 		if (x%i==0)
 			return false;
 	}
