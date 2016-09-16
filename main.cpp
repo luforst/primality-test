b3135a9e5e0ee74efd461bafeb53da2d00dc7a7a
Lukas Forst <lukas.forst@zoho.com>
2016-09-16 17:05:19 +0200

    Create main.cpp with roughly descr. of what it should do

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "primality-test.cpp"
+
+int main(int argc, char* argv)
+{
+	/* Call isPrimeBruteForce from primality-test.cpp for given numbers
+	 * Print out true or false for the given number or let main() return it */
+	return 0;
+}
