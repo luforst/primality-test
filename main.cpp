20b1242276eeb1308c8b7e0a73c3b05d16a67682
luforst <luforst@users.noreply.github.com>
2016-09-19 08:59:43 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "primality-test.cpp"
-
-int main(int argc, char* argv)
-{
-	/* Call isPrimeBruteForce from primality-test.cpp for given numbers
-	 * Print out true or false for the given number or let main() return it */
-	return 0;
-}
