b1a46af3e84c49b3f05083812a31300c4148920b
Lukas Forst <lukas.forst@zoho.com>
2017-09-04 10:58:26 +0200

    Replace c++ libraries by c standard libraries, make working for c language too

--- primality-test.cpp
+++ primality-test.cpp
@@ -1,24 +1,26 @@
-#include<cmath>
-#include<iostream>
+#include<math.h>
+#include<stdio.h>
+/*#include<iostream>
 
-using namespace std;
+using namespace std;*/
 
-bool isPrimeBruteForce(double x)
+int isPrimeBruteForce(int x)
 {
 	if (x < 2)
 		return false;
-	double sqroot_x = sqrt(x);
+	double sqroot_x;
+	sqroot_x = sqrt (x);
 	for(int i=0; i <= sqroot_x; i++) { /* If there were only factors above the square root of x, they would be bigger than x itself. */
 		if (x%i==0)
-			return false;
+			return 0;
 	}
-	return true;
+	return 1;
 }
 
 int main(int argc, char* argv[])
 {
-	double number;
-	bool result;
+	int result;
+/*	int number;
 	cout << "Enter number to test if it's prime: ";
 	cin >> number;
 	result = isPrimeBruteForce(number);
@@ -26,6 +28,8 @@ int main(int argc, char* argv[])
 		cout << number << " is a prime number.";
 	} else {
 		cout << number << " is not a prime number.";
-	}
+	}*/
+	result = isPrimeBruteForce(37);
+	printf("Is 37 a prime number? %i", result);
 	return 0;
 }
